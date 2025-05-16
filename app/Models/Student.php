d615b2414d983eec92a3d0282917aa7a131e0340
nahashon <nashonmwendwa0@gmail.com>
2025-05-16 17:19:45 +0300

    with this improvement one can view all students

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'course',
+        'registration_number',
+    ];
 }
